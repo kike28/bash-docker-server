6022527e56ecd34f32ee418c08538fb903e03010
Enrique Jiménez <enrique.jo.28@gmail.com>
2022-08-09 15:35:33 -0500

    Información para el usuario

--- config_core.sh
+++ config_core.sh
@@ -2,7 +2,8 @@
 
 echo '#########################################################'
 echo '#########################################################'
-echo 'Introduzca un nombre de usuario nuevo para manejar docker'
+echo 'Introduzca un nombre de usuario nuevo, que será el usuario'
+echo 'con los permisos para manejar docker en el sistema'
 echo '#########################################################'
 echo '#########################################################'
 
@@ -10,21 +11,20 @@ read usuarioDocker
 
 echo '#########################################################'
 echo '#########################################################'
-echo 'El usuario es $usuarioDocker'
+#echo 'El usuario es ' $usuarioDocker
 echo '#########################################################'
 echo '#########################################################'
 
 adduser $usuarioDocker
 usermod -aG docker $usuarioDocker
-#su $usuarioDocker
-#cd ~/
 git clone https://github.com/kike28/app-docker-server.git
 mv app-docker-server/core/ /opt/
-echo '#########################################################'
-echo '#########################################################'
-echo 'Ingresa un nuevo correo para Cambiar email del archivo traefik.yml'
-echo '#########################################################'
-echo '#########################################################'
+echo '###########################################################################'
+echo '###########################################################################'
+echo 'Ingresa el Correo para administracion de Certficado SSL '
+echo 'el archivo a configurar se encuentra en /opt/core/traefik-data/traefik.yml'
+echo '###########################################################################'
+echo '###########################################################################'
 
 read correoTraefik
 sed -i 's/email: tucorreo@mail.com/email: '$correoTraefik'/' /opt/core/traefik-data/traefik.yml
@@ -35,7 +35,7 @@ apt install -y apache2-utils
 # ESTE ES PARA GENERAR CONTRASE:A DE TRAEFIK
 echo '#########################################################'
 echo '#########################################################'
-echo 'Ahora vamos a generar la contrasena encriptada de tu traefik'
+echo 'Ahora vamos a generar la contrasena encriptada de traefik'
 echo 'Introduzca nombre de usuario de Traefik'
 echo '#########################################################'
 echo '#########################################################'
@@ -62,10 +62,17 @@ sed -i 's#'$contrasenaDefaulttraefik'#'$contrasenaTraefikfinal'#' /opt/core/trae
 
 
 chown $usuarioDocker:$usuarioDocker -R /opt/core/
-
 echo '#########################################################'
 echo '#########################################################'
-echo 'Tareas finalizadas con exito'
+echo 'La contraseña de traefik la puede encontrar en '
+echo '/opt/core/traefik-data/configurations/dynamic.yml'
+echo '#########################################################'
+echo '#########################################################'
+echo 'Tareas de configuración realizadas con exito'
+echo 'Ahora solo resta configurar el archivo docker-compose.yml'
+echo 'que se encuentra en la ruta /opt/core'
+echo 'cambia los nombre de dominio a los que necesites'
+echo 'luego corre en comando docker compose up -d'
 echo '#########################################################'
 echo '#########################################################'
 
